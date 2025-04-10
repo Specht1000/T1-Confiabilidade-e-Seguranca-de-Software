b8c9c3113214eab0ba549a6843721313dfd3ab09
Guilherme Specht <gmspecht27@gmail.com>
2025-04-09 21:01:31 -0300

    Error added

--- Unity/T1/TrianguloTest/src/triangulo.c
+++ Unity/T1/TrianguloTest/src/triangulo.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-const char* triangulo(int a, int b, int c) 
+const char* triangulo(int a, int b, int c)
 {
+    // cppcheck
+    // int d;
+    // if (d > 10) printf("Erro cppcheck\n"); // cppcheck acusa "uninitialized variable 'd'"
+
+    // if (0) {
+    //     printf("Nunca executa\n"); // cppcheck avisa código inatingível
+    // }
+    
+    // // valgrind
+    // int* vet = malloc(sizeof(int) * 3);
+    // vet[3] = 42; // acesso fora do vetor alocado (índice 3 é inválido)
+    // free(vet);
+
+    // int* ptr = malloc(sizeof(int) * 10); // vazamento de memória
+    // // esqueci o free(ptr);
+
+    // // address sanitizer
+    // int* x = malloc(sizeof(int));
+    // free(x);
+    // *x = 5; // uso após liberação
+
+    // int v[2];
+    // v[3] = 10; // <- estouro de buffer na stack
+
     // 1
     if (a < 1 || a > 200 || b < 1 || b > 200 || c < 1 || c > 200) // 2 
     {
@@ -27,12 +52,12 @@ const char* triangulo(int a, int b, int c)
     }
 }
 
-// int main ()
-// {
-//     int a, b, c;
+int main ()
+{
+    int a, b, c;
 
-//     printf("Defina os lados do triangulo: ");
-//     scanf("%d %d %d", &a, &b, &c);
+    printf("Defina os lados do triangulo: ");
+    scanf("%d %d %d", &a, &b, &c);
 
-//     triangulo(a, b, c);
-// }
+    triangulo(a, b, c);
+}
